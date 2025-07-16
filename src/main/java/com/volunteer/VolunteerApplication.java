b7220ffc249714e27690866b271bc6b263a4eed6
LuuHieuKhanh <124547427+LuuHieuKhanh@users.noreply.github.com>
2025-07-16 16:19:05 +0700

    feat: tạo base dự án + khởi tạo các chức năng cơ bản

--- src/main/java/com/volunteer/VolunteerApplication.java
+++ src/main/java/com/volunteer/VolunteerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VolunteerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VolunteerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerApplication.class, args);
+    }
 }
